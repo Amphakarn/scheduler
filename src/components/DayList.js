8ea490aad11e2fc97e0474d952ce92a609fa23a7
Amphakarn <pisuthigomol@yahoo.com>
2021-01-20 03:10:13 +0000

    Refactored DayList to make the coding style more consistent

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,19 +4,18 @@ import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
 
-  const arrDays = props.days.map(day => {
+  return (
+    <ul>{props.days.map(day =>
 
-    return (
-      <ul>
-        <DayListItem
-          key={day.id}
-          name={day.name} 
-          spots={day.spots} 
-          selected={day.name === props.day}
-          setDay={props.setDay}  
-        />
-      </ul>
-    );
-  });
-  return arrDays
-};
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+
+    )}
+    </ul>
+  );
+}
