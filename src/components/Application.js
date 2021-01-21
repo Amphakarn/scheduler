6069dde53a2ebec377df07c1dbaac172104e366e
Amphakarn <pisuthigomol@yahoo.com>
2021-01-21 22:15:53 +0000

    Finised Requesting the Days assignment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -52,27 +53,19 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+      .then(response => {
+        // console.log("*****RESPONSE = ", response);
+        setDays([...response.data])
+      })
+  }, [])
 
   return (
     <main className="layout">
@@ -98,9 +91,9 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointments.map(appointment => {
-          return <Appointment 
-            key={appointment.id} 
-            {...appointment} 
+          return <Appointment
+            key={appointment.id}
+            {...appointment}
           />
         })}
         <Appointment key="last" time="5pm" />
