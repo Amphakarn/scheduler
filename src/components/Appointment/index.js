f1f7aec52402ffdae2f66e05c1ee89dfa35cc54a
Amphakarn <pisuthigomol@yahoo.com>
2021-01-21 03:17:39 +0000

    Built and added state to appointment form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
 import "components/Appointment/styles.scss"
 
-export default function Appointment() {
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
+export default function Appointment(props) {
+
+  // console.log("***PROPS for Appointment = ", props);
 
   return (
-    <article className="appointment"></article>
-  )
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? 
+        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : 
+        <Empty />}
+    </article>
+  );
 }
