e93d86fd8c3c072256812f00f969454588746188
Amphakarn <pisuthigomol@yahoo.com>
2021-01-23 20:14:29 +0000

    Finished building a hook

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -9,3 +9,68 @@ test("useVisualMode should initialize with default value", () => {
 
   expect(result.current.mode).toBe(FIRST);
 });
+
+
+const SECOND = "SECOND";
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  // console.log("RESULT.all = ", result.all)
+  // console.log("RESULT.current = ", result.current)
+  // console.log("RESULT.transition = ", result.all.transition)
+
+
+  expect(result.current.mode).toBe(SECOND);
+});
+
+
+const THIRD = "THIRD";
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+    // console.log("RESULT.all = ", result.all, result.all.length)
+    // console.log("RESULT.current = ", result.current)
+    // console.log("RESULT.transition = ", result.all[0].transition)
+    // console.log("RESULT.back = ", result.all[0].back)
+
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+
+// Testing for Replace
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  console.log("RESULT.current = ", result.current.mode)
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  console.log("RESULT.current = ", result.current.mode)
+
+  expect(result.current.mode).toBe(FIRST);
+});
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,1 +1,43 @@
-export default function useVisualMode 
+import { useEffect, useState } from "react";
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode); // set mode
+    if (replace) {
+        setHistory(history.slice(0,-1), newMode); // replace last history with new mode
+        // console.log("MODE in transition = ", mode)
+        // console.log("HISTORY in transition = ", history)
+
+    } 
+    else {
+      setHistory(prev => ([...prev, newMode])); // add new mode to history
+        // console.log("MODE in (else) transition = ", mode)
+        // console.log("HISTORY in (else) transition = ", history)
+      
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[history.length-2]);
+        // console.log("MODE in if back= ", mode);
+
+      setHistory(prev => (history.slice(0, -1)));
+        // console.log("HISTORY in if back= ", history);
+
+    } 
+    else {
+      setMode(history[0]);
+        // console.log("MODE in else back= ", mode);
+      setHistory(mode);
+        // console.log("HISTORY in else back= ", history);
+
+    }
+  }
+
+  return { mode, transition, back };
+}
