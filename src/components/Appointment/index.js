5e502a184d229eadf98922a8f7bbd0c065e99a0c
Amphakarn <pisuthigomol@yahoo.com>
2021-01-24 02:32:38 +0000

    Finished up Custom Hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,63 @@ import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode.js"
+
 
 export default function Appointment(props) {
 
-  // console.log("***PROPS for Appointment = ", props);
+  console.log("***PROPS for Appointment = ", props);
+
+  // mode
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  const onAdd = () => transition(CREATE);
+
+  const onSave = (name, interviewer) => {
+    transition(SHOW);
+  }
+
+  // const onCancel = () => {
+  //   back()
+  // }
+
+  // console.log("MODE = ", mode)
+
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? 
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : 
-        <Empty />}
+      { mode === EMPTY &&
+        <Empty onAdd={onAdd} />}
+
+      { mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )
+      }
+
+      { mode === CREATE && (
+        <Form
+          name={"Bee"}
+          interviewer={null}
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={onSave}
+        />
+      )}
+
+      {/* {props.interview ?
+        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
+        <Empty />} */}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -9,33 +9,20 @@ export default function useVisualMode(initial) {
     setMode(newMode); // set mode
     if (replace) {
         setHistory(history.slice(0,-1), newMode); // replace last history with new mode
-        // console.log("MODE in transition = ", mode)
-        // console.log("HISTORY in transition = ", history)
-
     } 
     else {
-      setHistory(prev => ([...prev, newMode])); // add new mode to history
-        // console.log("MODE in (else) transition = ", mode)
-        // console.log("HISTORY in (else) transition = ", history)
-      
+      setHistory(prev => ([...prev, newMode])); // add new mode to history      
     }
   }
 
   const back = () => {
     if (history.length > 1) {
       setMode(history[history.length-2]);
-        // console.log("MODE in if back= ", mode);
-
       setHistory(prev => (history.slice(0, -1)));
-        // console.log("HISTORY in if back= ", history);
-
     } 
     else {
       setMode(history[0]);
-        // console.log("MODE in else back= ", mode);
       setHistory(mode);
-        // console.log("HISTORY in else back= ", history);
-
     }
   }
 
